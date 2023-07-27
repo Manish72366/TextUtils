f715c2d8e1900df990ec3902cf7224b3b99d0d7c
Manish <manishmamgain7236672366@gmail.com>
2023-07-27 14:08:08 +0530

    Props and modules , import , export , proptypes default etc

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
-import "./manish.css";
-// 1. In computer programming, a library refers to a collection of files, programs, routines, scripts, or functions that can be referenced in the programming code. These libraries are more complex than routines, optimized, and may even be designed by the same programmers behind the programming language.
-// A software framework is a structure that you can use to build software. It acts as a foundation so you don't have to deal with creating unnecessary extra logic from scratch.
-let name = "<b>manish</b>"; // This will not look like bold this will look same as a string written  .
+import "./App.css";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">  {/* class = className in the react and some changes are also  for = HtmlFor , tab = Tab and so other*/}
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtils</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a> {/* href = "/" means link to the same page where you are */}
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li> {/* non closing tags like hr should close by /> */}
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-</>
+      {/* So we are importing Navbar from Navbar.js so the thing which that file returns has been shown here */}
+      <Navbar title="textUtils" aboutText="About TextUtils" />
+      {/* <Navbar title = {7} aboutText = "about"/>  this show an error in console as title is integer*/}
+      <Navbar /> {/* Default props values will be assigned*/}
+      <Navbar title = "manish"/>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+// rfc  a shortuct for react function based components
+// In React, the term props stands for properties, which refers to the properties of an object. This is because all elements created in React are JavaScript objects. As a result, passing data into a component is done by creating properties and values for the object.
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Navbar(props) {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">  {/* class = className in the react and some changes are also  for = HtmlFor , tab = Tab and so other*/}
+    <div className="container-fluid">
+      <a className="navbar-brand" href="/">{props.title}</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">{props.aboutText}</a> {/* href = "/" means link to the same page where you are */}
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="/">Action</a></li>
+              <li><a className="dropdown-item" href="/">Another action</a></li>
+              <li><hr className="dropdown-divider"/></li> {/* non closing tags like hr should close by /> */}
+              <li><a className="dropdown-item" href="/">Something else here</a></li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form className="d-flex" role="search">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  )
+}
+
+// To define what are the data types of props you are going to send if that are been different than assigned so show an error in console . other wise error won't show in console.  so glti pakdi jaaye we defined this.
+Navbar.propTypes = {
+    title : PropTypes.string,
+    // title : PropTypes.string.isRequired, means if you forget to send the title so it will show you an error in console.
+    aboutText : PropTypes.string.isRequired
+};
+
+Navbar.defaultProps = {
+     title : "Set krna bhai title",
+     aboutText : "About"
+}
