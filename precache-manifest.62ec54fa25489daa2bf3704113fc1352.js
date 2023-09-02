1287e63bfdeb50f440ad065dad2ceee3171351ce
Manish <manishmamgain7236672366@gmail.com>
2023-09-03 04:18:03 +0530

    Updates

---
+++ precache-manifest.62ec54fa25489daa2bf3704113fc1352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74fe79e3d874f8aa7c8f",
+    "url": "/TextUtils/static/css/main.bdada1bb.chunk.css"
+  },
+  {
+    "revision": "74fe79e3d874f8aa7c8f",
+    "url": "/TextUtils/static/js/main.bf690a20.chunk.js"
+  },
+  {
+    "revision": "b36f217a3f468d9139b5",
+    "url": "/TextUtils/static/js/runtime~main.8d016058.js"
+  },
+  {
+    "revision": "cdd919d56cec3edf64d7",
+    "url": "/TextUtils/static/js/2.1b5716a2.chunk.js"
+  },
+  {
+    "revision": "5854eb9be6dd082bd048",
+    "url": "/TextUtils/static/js/3.524ce66f.chunk.js"
+  },
+  {
+    "revision": "84b7b8175d0c19eabc5220b732cda07d",
+    "url": "/TextUtils/index.html"
+  }
+];
